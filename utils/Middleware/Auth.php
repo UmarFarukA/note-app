7dc8b35d1d1b40c2e74d48060c1c7d0183968770
Umar Faruk Abdulhamid <talk2ufaz@gmail.com>
2023-07-13 14:39:13 +0100

    refactoring

--- utils/Middleware/Auth.php
+++ utils/Middleware/Auth.php
@@ -7,7 +7,7 @@ class Auth
     public function handle()
     {
         if (!$_SESSION['user'] ?? false) {
-            header('location: /');
+            redirect('/');
             exit();
         }
     }
